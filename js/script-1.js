effc8489d6fa134eb333eecce1e354cebe88eaae
Vitaliy Bashkir <vabashkir@icloud.com>
2025-07-25 06:04:49 +0300

    add all html and js

--- js/script-1.js
+++ js/script-1.js
@@ -1,13 +1,9 @@
-// Отримуємо посилання на список категорій
 const categoriesList = document.querySelector("#categories");
 
-// Знаходимо всі елементи з класом "item"
 const categoryItems = categoriesList.querySelectorAll(".item");
 
-// Виводимо кількість категорій
 console.log(`Number of categories: ${categoryItems.length}`);
 
-// Обходимо кожен елемент категорії
 categoryItems.forEach(item => {
   const categoryName = item.querySelector("h2").textContent;
   const categoryElementsCount = item.querySelectorAll("ul li").length;
